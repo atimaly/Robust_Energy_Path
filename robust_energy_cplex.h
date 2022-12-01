896e74353e95811bb008c8eaae4c5646618b4f3f
atimaly <atimaly@gmail.com>
2022-12-02 00:35:07 +0100

    Working one iteration for basic loop

--- robust_energy_cplex.h
+++ robust_energy_cplex.h
@@ -52,46 +52,20 @@ class Paths : public Graph {
 		pair<int,int> RandomPath();
 		
 		void RandomPaths();
-		
-		
-		void Perturbation();
-		/*
-		void upper_add_line(double min_l, double max_l, map<int, Mip::Col> &f, Mip &mip);
 
-		void lower_add_line(double min_l, map<int, Mip::Col> &f, Mip &mip);
-		
-		void subset_upper_value(double prob_in_subset, double upper_value, vector<vector<int>> &defining_polyhedra, map<int, Lp::Col> &f, Lp &lp, std::ostream &os = std::cerr);
+		void PerturbationOfq(vector<double> &q_tariff, const double delta, std::ostream &os = std::cerr) const;
 
-		void CreateUpperLowerPolyhedra(map<int, Mip::Col> &f, Mip &mip);
-		
-		void CreateSubsetPolyhedra(int many_ineq_q, double prob_in_subset, double upper_value, vector<vector<int>> &defining_polyhedra, map<int, Lp::Col> &f, Lp &lp, std::ostream &os = std::cerr);
-		
-		void SettingQValue(int many_ineq_q, double prob_q, double max_q, std::ostream &os = std::cerr);
-		
-		void CreatingUPolyhedra(int many_ineq_u, double prob_u, double max_u, std::ostream &os = std::cerr);
-		
-		void IpSolve34(vector<double> &q_tariff, int big_M);
-		
-		void IPSolve35(int big_M);
-		
-		void FindingOptimalCost(int many_ineq_q, double prob_q, double max_q, int many_ineq_u, double prob_u, double max_u, std::ostream &os = std::cerr);
-		*/
-
-		//void CreateSubsetPolyhedra(int many_ineq_q, double prob_in_subset, double upper_value, vector<vector<int>> &defining_polyhedra, std::ostream &os = std::cerr);
-
-		//void CreatingUPolyhedra(int many_ineq_u, double prob_u, double max_u, std::ostream &os = std::cerr);
-
-		void InitialQValue(std::ostream &os = std::cerr);
+		void InitialQValue(vector<double> &q_tariff, std::ostream &os = std::cerr) const;
 
 		double MinimizeLeadersEarning(const vector<double> &q_tariff, const int big_M, std::ostream &os = std::cerr);
 
-		void FindingTariffWithFiniteUtilities(const int big_M, std::ostream &os = std::cerr);
+		double FindingTariffWithFiniteUtilities(const int big_M, std::ostream &os = std::cerr);
 
 	public:
 
 		Paths(const int people, const int n, const double erdos_p);
 
-		Paths(std::istream &is); //You read the input more specially D graph, paths of the people, arc_cost_buy_p, Q polyhedra, U polyhedra
+		Paths(std::istream &is); //You read the input, more specifically D graph, paths of the people, arc_cost_buy_p, Q polyhedra, U polyhedra
 
         ~Paths();
 
@@ -122,7 +96,7 @@ const bool DEBUG = true;
 #define FOR(i,n) for(int i = 0; i < (n); ++i)
 #define FORO(i,n) for(int i = 1; i < (n); ++i)
 
-template <class C>
+template <typename C>
 void Print_vector(const C &Original, std::ostream &os = std::cerr) {
 	for(const auto &v : Original) {
 	    os << v << " ";
@@ -130,8 +104,8 @@ void Print_vector(const C &Original, std::ostream &os = std::cerr) {
 	os << endl;
 }
 
-template <class C>
-void Print_Matrix(const vector<vector<C>> &M, std::ostream &os = std::cerr) {
+template <typename C>
+void Print_Matrix(const C &M, std::ostream &os = std::cerr) {
 	for(auto &v : M) {
 		os << v.size() << " ";
 		for(auto &u : v) {
